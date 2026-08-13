cc947df155b4ecd649523de527f855e7c1334753
Alex Sander <alex.sdmg@hotmail.com>
2026-08-12 21:49:15 -0400

    fix: add test redirect

--- internal/handler/url_handler_integration_test.go
+++ internal/handler/url_handler_integration_test.go
@@ -3,11 +3,14 @@ package handler
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"shortener/internal/config"
 	"shortener/internal/database"
+	"shortener/internal/model"
 	"shortener/internal/repository"
 	"shortener/internal/service"
 	"testing"
@@ -92,4 +95,137 @@ func TestURLHandler_CreateIntegration(t *testing.T) {
 			response.StatusCode,
 		)
 	}
+}
+
+func TestURLHandler_RedirectIntegration(t *testing.T) {
+
+	cfg, err := config.Load()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db, err := database.NewPostgres(
+		cfg.Database,
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
+
+	urlRepository := repository.NewURLRepository(db)
+
+	urlService := service.NewURLService(
+		urlRepository,
+	)
+
+	urlHandler := NewURLHandler(
+		urlService,
+		cfg.App.BaseURL,
+	)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(
+		"GET /{code}",
+		urlHandler.Redirect,
+	)
+
+	server := httptest.NewServer(mux)
+
+	defer server.Close()
+
+	ctx := context.Background()
+
+	testURL := &model.URL{
+		Code:        testCode(t),
+		OriginalURL: "https://google.com",
+	}
+
+	err = urlRepository.Create(
+		ctx,
+		testURL,
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request, err := http.NewRequest(
+		http.MethodGet,
+		server.URL+"/"+testURL.Code,
+		nil,
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client := &http.Client{
+		CheckRedirect: func(
+			req *http.Request,
+			via []*http.Request,
+		) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusFound {
+
+		t.Fatalf(
+			"expected status %d, got %d",
+			http.StatusFound,
+			response.StatusCode,
+		)
+	}
+
+	location := response.Header.Get("Location")
+
+	if location != testURL.OriginalURL {
+
+		t.Fatalf(
+			"expected Location %s, got %s",
+			testURL.OriginalURL,
+			location,
+		)
+	}
+
+	foundURL, err := urlRepository.FindByCode(
+		ctx,
+		testURL.Code,
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if foundURL.Clicks != 1 {
+
+		t.Fatalf(
+			"expected 1 click, got %d",
+			foundURL.Clicks,
+		)
+	}
+}
+
+func testCode(t *testing.T) string {
+	t.Helper()
+
+	bytes := make([]byte, 4)
+
+	if _, err := rand.Read(bytes); err != nil {
+		t.Fatal(err)
+	}
+
+	return hex.EncodeToString(bytes)[:6]
 }
